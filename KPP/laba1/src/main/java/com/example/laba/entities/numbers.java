b5711df993400c85e678b60d2dd5fa6dab4f781b
tembane <v.okh02@gmail.com>
2022-02-27 17:53:13 +0300

    laba1 completed

---
+++ KPP/laba1/src/main/java/com/example/laba/entities/numbers.java
@@ -0,0 +1,28 @@
+package com.example.laba.entities;
+
+public class numbers {
+
+    private String result;
+    private int number;
+
+    public numbers(String result, int number) {
+        this.result = result;
+        this.number = number;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
